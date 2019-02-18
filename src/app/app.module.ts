b43516238db81da7202c157b67d71b5fe7e0ef90
Kate <kateryna.bohatova@gmail.com>
2019-02-19 00:37:19 +0200

    fc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UserModule } from './user/user.module';
+import { LoginModule } from './login/login.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
